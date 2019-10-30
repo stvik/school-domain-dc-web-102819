6e79cd30370b44af4435c430635b94e086dd4ad6
Flatiron School <stvik2@gmail.com>
2019-10-30 17:08:29 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+
+class School
+
+    attr_reader :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student_name,grade)
+        if @roster.include?(grade)
+            @roster[grade] <<student_name
+        else
+            @roster[grade] = []
+            @roster[grade] << student_name
+        end
+    end
+
+    def grade(grade)
+        return @roster[grade]
+    end
+
+    def sort
+        new_hash = {}
+        @roster.each do |grade,students|
+            new_hash[grade] = students.sort
+        end
+        return new_hash
+    end
+end
